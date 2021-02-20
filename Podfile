036c02231f59d0c33b693aabc1941876aef1b65e
vkalil <vkalil@wustl.edu>
2021-02-20 14:54:53 -0500

    Firebase Pods, Successful Connection, Security Initialization

---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SupremeNow' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SupremeNow
+pod 'Firebase/Analytics'
+pod 'Firebase/Firestore'
+pod 'Firebase/Crashlytics'
+pod 'Firebase/Auth'
+pod 'Firebase/Functions'
+pod 'Firebase/Storage'
+
+  target 'SupremeNowTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SupremeNowUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ SupremeNow/FeedViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FeedViewController.swift
+//  SupremeNow
+//
+//  Created by Victor Kalil on 2/20/21.
+//
+
+import UIKit
+import FirebaseFirestore
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let db = Firestore.firestore()
+        let blah = db.collection("users").document("testID")
+        blah.getDocument { (document, error) in
+            if let document = document, document.exists {
+               // let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                
+                let userInfo = document.data()!["info"]! as? String ?? "None"
+                print(userInfo)
+            }else {
+                print("Document does not exist")
+            }
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
