f4da8581208a026e5511f2954b81b1622e3dac10
Akshat Sharma <akshatksharma@wustl.edu>
2021-02-22 10:56:44 -0600

    Adding gitignore and pods

---
+++ Pods/Target Support Files/Pods-SupremeNow-SupremeNowUITests/Pods-SupremeNow-SupremeNowUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SupremeNow_SupremeNowUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SupremeNow_SupremeNowUITests
+@end
---
+++ Pods/Target Support Files/Pods-SupremeNowTests/Pods-SupremeNowTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SupremeNowTests : NSObject
+@end
+@implementation PodsDummy_Pods_SupremeNowTests
+@end
