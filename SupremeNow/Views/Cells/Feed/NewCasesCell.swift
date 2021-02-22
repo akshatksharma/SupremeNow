c7d78171f62aa8660855c8372393a0b0ccc37604
Akshat Sharma <akshatksharma@wustl.edu>
2021-02-22 11:27:12 -0600

    Making Feed into tableview implementation

---
+++ SupremeNow/Views/Cells/Feed/NewCasesCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewCasesCell.swift
+//  SupremeNow
+//
+//  Created by Akshat Sharma on 2/22/21.
+//
+
+import UIKit
+
+class NewCasesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
